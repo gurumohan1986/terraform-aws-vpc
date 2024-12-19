8b93e7c342b56a64e2a05169d80c7bdfa866accf
Gurumohan <guru.eternal@gmail.com>
2024-12-19 18:11:30 +0000

    added the region on the variables file

--- provider.tf
+++ provider.tf
@@ -15,7 +15,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "AWS_ACCESS_KEY_ID" {
   description = "AWS access key"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
   description = "AWS secret access key"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "aws_region" {
   description = "Region of the AWS where you want to privision infrastructure"
   type        = string
-  default     = "" #"us-east-1"
+  default     = "us-east-1"
 }
 
 variable "cidr_block" {
   description = "CIDR range for the VPC that you wanted to create"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "main_vpc_tags" {
   description = "Tags for the VPC that you wanted to create"
-  type = map
-  default = {}
+  type        = map(any)
+  default     = {}
 }
