a4dea922e669dea84081f3265c16adf66fc11d4f
Gurumohan <guru.eternal@gmail.com>
2024-12-20 11:59:00 +0000

    changes

--- output.tf
+++ output.tf
@@ -2,14 +2,6 @@
 #   value = aws_vpc.main
 # }
 
-output "access_key" {
-  value = var.AWS_ACCESS_KEY_ID
-}
-
-output "secret_key" {
-  value = var.AWS_SECRET_ACCESS_KEY
-}
-
 output "aws_region" {
   value = var.aws_region
 }
--- provider.tf
+++ provider.tf
@@ -1,11 +1,20 @@
 terraform {
   required_version = ">= 1.1.0"
+
   cloud {
     organization = "mudragiri"
     workspaces {
       tags = "aws0_useast_1_vpc"
     }
   }
+
+  # backend "hcp" {
+  #   # Specify your HCP configuration, for example:
+  #   hostname  = "app.hcp.hashicorp.com"  # HCP's endpoint URL (should not be empty)
+  #   api_token = "your-hcp-api-token"     # Token to authenticate with HCP
+  #   region    = "us-west"                 # Specify your HCP region if necessary
+  # }
+
   required_providers {
     # aws = {
     #   source  = "hashicorp/aws"
@@ -25,11 +34,4 @@ terraform {
 #   # access_key = var.AWS_ACCESS_KEY_ID
 #   # secret_key = var.AWS_SECRET_ACCESS_KEY
 # }
-
-# Debugging: Ensure AWS credentials are available
-resource "null_resource" "debug_aws_creds" {
-  provisioner "local-exec" {
-    command = "echo AWS_ACCESS_KEY_ID=${var.AWS_ACCESS_KEY_ID} && echo AWS_SECRET_ACCESS_KEY=${var.AWS_SECRET_ACCESS_KEY}"
-  }
-}
 
