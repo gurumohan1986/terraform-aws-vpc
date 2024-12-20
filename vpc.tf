5f6b6f72df242c45f0f846bf2c6e27b690c88dfb
Gurumohan <guru.eternal@gmail.com>
2024-12-20 16:47:57 +0000

    correction

--- vpc.tf
+++ vpc.tf
@@ -20,7 +20,8 @@ resource "aws_security_group" "allow_ssh" {
   ingress {
     cidr_blocks = [
       "0.0.0.0/0"
-    ]from_port = 22
+    ]
+    from_port = 22
     to_port = 22
     protocol = "tcp"
   }// Terraform removes the default rule
